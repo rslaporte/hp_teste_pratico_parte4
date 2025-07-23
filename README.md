8dc0302fd7f6fb60540e250c04e30c0a374aa16e
rslaporte <rafanex@gmail.com>
2025-07-23 03:24:28 -0300

    feat: Adding the Open-Meteo get request and save the data into a .xlsx file

--- README.md
+++ README.md
@@ -1,2 +1,50 @@
-# hp_teste_pratico_parte4
-Repositório contendo os dados da PARTE 4 do teste da House Performance, conectando e extraindo os dados de uma API pública.
+====================================================
+  CONSULTA DE CLIMA - OPEN-METEO (últimos 7 dias)
+====================================================
+
+Este projeto faz parte da resolução da parte 4 do teste da House Performance. O script em Python consulta a API pública Open-Meteo e exporta alguns dados meteorológicos dos últimos 7 dias para um arquivo Excel (.xlsx) local.
+
+----------------------------------------
+REQUISITOS:
+----------------------------------------
+
+- Python 3.8 ou superior
+- Acesso à internet
+
+----------------------------------------
+PASSO A PASSO PARA EXECUÇÃO:
+----------------------------------------
+
+1. Abra o terminal na pasta onde estão os arquivos.
+
+2. Crie e ative um ambiente virtual:
+
+   - Windows:
+       python -m venv venv
+       venv\Scripts\activate
+
+   - macOS / Linux:
+       python3 -m venv venv
+       source venv/bin/activate
+
+3. Instale as dependências:
+
+       pip install -r requirements.txt
+
+4. Execute o script:
+
+       python app.py
+
+----------------------------------------
+RESULTADO:
+----------------------------------------
+
+O script criará um arquivo chamado "open_meteo_data.xlsx" contendo os dados climáticos horários dos últimos 7 dias para as localidades de Franca, Sâo Paulo, Porto Alegre e Rio de Janeiro
+
+----------------------------------------
+OBSERVAÇÕES:
+----------------------------------------
+
+- O script já define as latitudes e longitudes.
+- Os dados são obtidos diretamente da API pública do Open-Meteo https://open-meteo.com/
+- O arquivo de saída será salvo na mesma pasta do projeto.
---
+++ app.py
@@ -0,0 +1,77 @@
+#%%
+import requests
+import pandas as pd
+from datetime import datetime, timedelta
+
+#Classe para gerenciar a Open-Meteo API. Esta API pode ser encontrada em https://open-meteo.com/en/docs
+class OpenMeteo:
+    def __init__(self):
+        self.url = "https://archive-api.open-meteo.com/v1/archive"
+
+    def get_data(self, lat, long, city, state):
+        params = {
+            "latitude": lat,
+            "longitude": long,
+            "end_date": datetime.today().date() - timedelta(days=2), #Fiz essa requisição em 23-07-2025. Neste dia, não haviam dados referentes aos dias 22 e 23 deste mesmo mês. Por este motivo, considerei 22-07-2025 como data final.
+            "start_date": datetime.today().date() - timedelta(days=8), #7 dias antes da data final, incluindo esta última.
+            "hourly": "temperature_2m,precipitation,cloudcover,relative_humidity_2m,rain,wind_speed_10m", #Métricas que aparecerão na tabela final, i.e. o dado que será extraído.
+            "timezone": "America/Sao_Paulo" 
+        }
+
+        #Extraindo os dados usando uma requisição GET
+        response = requests.get(self.url, params=params)
+
+        #Formatando a resposta em no formato JSON
+        data = response.json()
+
+        #Transformando os dados em um DataFrame. É mais fácil manipular os dados dessa forma.
+        df = pd.DataFrame(data['hourly']) #Extraindo todas as métricas
+        df['time'] = pd.to_datetime(df['time']) # Formatando a coluna 'time'
+        df['city'] = city #Setando uma dimensão para cidade
+        df['state'] = state
+
+        return df
+    
+    def save_data(self, df, file_name):
+        df.to_excel(file_name, index=False)
+
+#Algumas localizações escolhidas aleatoriamente para compor os dados
+locations = [
+    {
+        "lat": -20.5386,
+        "long": -47.4008,
+        "city": "Franca",
+        "state": "SP"
+    },
+    {
+        "lat": -23.5475,
+        "long": -46.6361,
+        "city": "Sao Paulo",
+        "state": "SP"
+    },
+    {
+        "lat": -30.0328,
+        "long": -51.2302,
+        "city": "Porto Alegre",
+        "state": "RS"
+    },
+    {
+        "lat": -22.9064,
+        "long": -43.1822,
+        "city": "Rio de Janeiro",
+        "state": "RJ"
+    }
+]
+
+#Inicializando um DataFrame para receber todos os dados
+df = pd.DataFrame({})
+
+#Gerando o DataFrame final extraindo todos os dados de todas as localizações
+for location in locations:
+    df_location = OpenMeteo().get_data(**location)
+    df = pd.concat([df, df_location]).reset_index(drop=True)
+
+#Salvando os dados em formato
+OpenMeteo().save_data(df, 'open_meteo_data.xlsx')
+
+
